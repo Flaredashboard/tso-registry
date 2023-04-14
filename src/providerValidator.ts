506b735007e2ed5a89fd0fd46c6eee6d5ce6f6f8
Erik <emrbeltane@gmail.com>
2023-04-14 14:47:35 +0200

    Validation refactor, validate addresses from file

--- src/providerValidator.ts
+++ src/providerValidator.ts
@@ -1,29 +1,7 @@
-import Ajv from "ajv";
 import yargs from "yargs";
-import * as fs from "fs"
+import { Validator } from "./Validator";
 
-async function validateFileFormat(fileName: string) {
-  const ajv = new Ajv();
-  const schema = JSON.parse(fs.readFileSync("src/singleProviderSchema.json", {encoding:'utf8', flag:'r'}))
-
-  if(!fs.existsSync(fileName)){
-    throw new Error(`No such file: "${fileName}"`)
-  }
-
-  const data =  JSON.parse(fs.readFileSync(fileName, {encoding:'utf8', flag:'r'}))
-
-  const validate = ajv.compile(schema);
-  const valid = validate(data);
-
-  if (valid) {
-    console.log("Valid format of TSO provider file");
-    return true;
-  }
-
-  throw new Error(`Invalid format of TSO provider file "${fileName}"`)
-}
-
-const { argv } = yargs.scriptName("Validate provider file").option("f", {
+const { argv } = yargs.scriptName("Validate provider file format").option("f", {
   alias: "provider-file",
   describe: "Path to provider file",
   demandOption: "Provider file is required",
@@ -34,7 +12,8 @@ const { argv } = yargs.scriptName("Validate provider file").option("f", {
 // @ts-ignore
 const { providerFile } = argv;
 
-validateFileFormat(providerFile)
+new Validator()
+  .validateFileFormat(providerFile)
   .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
