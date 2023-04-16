f6af3984b6f623702d7fbb4ef8b3898d958d0f64
Erik <emrbeltane@gmail.com>
2023-04-16 14:14:48 +0200

    Check whitelisted providers, added API key and token

--- src/Validator.ts
+++ src/Validator.ts
@@ -1,10 +1,29 @@
 import axios, { AxiosResponse } from "axios";
 import Ajv from "ajv";
 import Web3 from "web3";
+import { AbiItem } from "web3-utils";
 import path from "path";
 import * as fs from "fs";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export class Validator {
+  githubConfig = {
+    headers: {
+      Authorization: `Bearer ${process.env.GITHUB_API_TOKEN}`,
+    },
+  };
+
+  flareConfig = {
+    headers: {
+      "x-apikey": process.env.FLARE_API_KEY,
+    },
+  };
+
+  private getRegistryAddress() {
+    return "0xaD67FE66660Fb8dFE9d6b1b4240d8650e30F6019";
+  }
+
   private getRpcFromChainId(chainId: number) {
     switch (chainId) {
       case 14:
@@ -17,6 +36,155 @@ export class Validator {
         return "https://coston2-api.flare.network/ext/C/rpc";
       default:
         throw new Error(`Invalid chain id: ${chainId}`);
+    }
+  }
+
+  private getApiEndpointFromChainId(chainId: number) {
+    switch (chainId) {
+      case 14:
+        return "https://api.flare.network/flare-explorer/api";
+      case 16:
+        return "https://api.flare.network/coston-explorer/api";
+      case 19:
+        return "https://api.flare.network/songbird-explorer/api";
+      case 114:
+        return "https://api.flare.network/coston2-explorer/api";
+      default:
+        throw new Error(`Invalid chain id: ${chainId}`);
+    }
+  }
+
+  private appendParametersToUrl(url: string, params: Object) {
+    let endpoint = new URL(url);
+
+    for (const [key, value] of Object.entries(params)) {
+      endpoint.searchParams.set(key, value);
+    }
+
+    return endpoint.toString();
+  }
+
+  private async checkProviderIsWhitelisted(chainId: number, address: string) {
+    const assets = await this.getSupportedAssets(chainId);
+
+    for (const asset of assets) {
+      let providers = await this.getWhitelistedProvidersForAsset(
+        chainId,
+        asset
+      );
+      if (providers.includes(address)) return true;
+    }
+
+    throw new Error(
+      `Address ${address} is not whitelisted for any asset on chain ${chainId}`
+    );
+  }
+
+  private async getWhitelistedProvidersForAsset(
+    chainId: number,
+    asset: string
+  ) {
+    try {
+      const address = await this.getAddressFromContractName(
+        chainId,
+        "VoterWhitelister"
+      );
+      const contract = await this.createContract(chainId, address);
+
+      return await contract.methods
+        .getFtsoWhitelistedPriceProvidersBySymbol(asset)
+        .call();
+    } catch (error) {
+      throw new Error(
+        `getWhitelistedProvidersForAsset ${asset} failed: ${error}`
+      );
+    }
+  }
+
+  async getSupportedAssets(chainId: number) {
+    try {
+      const address = await this.getAddressFromContractName(
+        chainId,
+        "FtsoRegistry"
+      );
+      const contract = await this.createContract(chainId, address);
+
+      return await contract.methods.getSupportedSymbols().call();
+    } catch (error) {
+      throw new Error(`getSupportedAssets failed: ${error}`);
+    }
+  }
+
+  private async createContract(chainId: number, address: string) {
+    const rpc = this.getRpcFromChainId(chainId);
+    const web3 = new Web3(rpc);
+
+    let params = {
+      address: address,
+      module: "contract",
+      action: "getabi",
+    };
+
+    try {
+      let isProxy = false;
+      let abi = await this.getAbi(chainId, params);
+
+      for (let method of abi) {
+        if (
+          method.stateMutability === "view" &&
+          method.name === "implementation"
+        ) {
+          isProxy = true;
+          break;
+        }
+      }
+
+      if (isProxy) {
+        const proxyAddress = await new web3.eth.Contract(abi, address).methods
+          .implementation()
+          .call();
+
+        params.address = proxyAddress;
+        abi = await this.getAbi(chainId, params);
+      }
+
+      return new web3.eth.Contract(abi, address);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getAbi(chainId: number, params: Object): Promise<AbiItem[]> {
+    const endpoint = this.getApiEndpointFromChainId(chainId);
+    const url = this.appendParametersToUrl(endpoint, params);
+
+    try {
+      const response = await axios.get(url, this.flareConfig);
+      return JSON.parse(response.data.result);
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  private async getAddressFromContractName(
+    chainId: number,
+    contractName: string
+  ) {
+    try {
+      const registryContract = await this.getRegistry(chainId);
+      return await registryContract.methods
+        .getContractAddressByName(contractName)
+        .call();
+    } catch (error) {
+      throw new Error(`getAddressFromContractName failed: ${error}`);
+    }
+  }
+
+  private async getRegistry(chainId: number) {
+    try {
+      return await this.createContract(chainId, this.getRegistryAddress());
+    } catch (error) {
+      throw new Error(`getRegistry failed: ${error}`);
     }
   }
 
@@ -36,7 +204,9 @@ export class Validator {
       for (const item of data) {
         if (item.status === "fulfilled") return true;
       }
-      throw new Error(`User ${username} does not have authorization to delete ${filename}`);
+      throw new Error(
+        `User ${username} does not have authorization to delete ${filename}`
+      );
     } catch (error: any) {
       throw new Error(error);
     }
@@ -74,6 +244,11 @@ export class Validator {
           file.ftso_info.address,
           file.ftso_info.chainId
         );
+
+        await this.checkProviderIsWhitelisted(
+          file.ftso_info.chainId,
+          file.ftso_info.address
+        );
       }
       if ("stso_info" in file) {
         if (!addressNameValidated && addressName === file.stso_info.address) {
@@ -87,9 +262,14 @@ export class Validator {
           file.stso_info.address,
           file.stso_info.chainId
         );
+
+        await this.checkProviderIsWhitelisted(
+          file.stso_info.chainId,
+          file.stso_info.address
+        );
       }
-      if ("coston_info" in file) {
-        if (!addressNameValidated && addressName === file.coston_info.address) {
+      if ("ctso_info" in file) {
+        if (!addressNameValidated && addressName === file.ctso_info.address) {
           addressNameValidated = true;
         } else {
           throw new Error("Filename address does not match Coston");
@@ -97,15 +277,17 @@ export class Validator {
 
         await this.checkIdWithSmartContract(
           userId,
-          file.coston_info.address,
-          file.coston_info.chainId
+          file.ctso_info.address,
+          file.ctso_info.chainId
+        );
+
+        await this.checkProviderIsWhitelisted(
+          file.ctso_info.chainId,
+          file.ctso_info.address
         );
       }
-      if ("coston2_info" in file) {
-        if (
-          !addressNameValidated &&
-          addressName === file.coston2_info.address
-        ) {
+      if ("c2tso_info" in file) {
+        if (!addressNameValidated && addressName === file.c2tso_info.address) {
           addressNameValidated = true;
         } else {
           throw new Error("Filename address does not match Coston2");
@@ -113,8 +295,13 @@ export class Validator {
 
         await this.checkIdWithSmartContract(
           userId,
-          file.coston2_info.address,
-          file.coston2_info.chainId
+          file.c2tso_info.address,
+          file.c2tso_info.chainId
+        );
+
+        await this.checkProviderIsWhitelisted(
+          file.c2tso_info.chainId,
+          file.c2tso_info.address
         );
       }
     } catch (error: any) {
@@ -127,7 +314,8 @@ export class Validator {
   public async getUserIdFromUsername(username: string): Promise<any> {
     try {
       const response: AxiosResponse = await axios.get(
-        `https://api.github.com/users/${username}`
+        `https://api.github.com/users/${username}`,
+        this.githubConfig
       );
 
       const user = response.data;
